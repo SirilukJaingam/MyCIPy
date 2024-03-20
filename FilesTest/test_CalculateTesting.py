2bce130fffa5fde1936b48d67915ac0ec791b888
Siriluk007 <siriluk.jaingam@dexon-technology.com>
2024-03-20 15:09:16 +0700

    correct

--- FilesTest/test_CalculateTesting.py
+++ FilesTest/test_CalculateTesting.py
@@ -7,13 +7,13 @@ class TestCalculator:
 
 
     def test_addition_secon(self):
-        assert 6 == Calculate.add(3, 2)
+        assert 5 == Calculate.add(3, 2)
 
     def test_subtraction(self):
         assert 2 == Calculate.subtract(4, 2)
 
     def test_ErrorAddition(self):
-        assert 6 == Calculate.add(2, 2)
+        assert 4 == Calculate.add(2, 2)
 
     def test_subtraction(self):
         assert 2 == Calculate.subtract(4, 2)
