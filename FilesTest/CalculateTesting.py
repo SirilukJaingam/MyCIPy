fee860908ac9d1d6ac43aaad4afc5d0796ff4bd0
Siriluk007 <siriluk.jaingam@dexon-technology.com>
2024-03-20 14:50:14 +0700

    update

--- FilesTest/CalculateTesting.py
+++
@@ -1,9 +0,0 @@
-import Calculate
-
-class TestCalculator:
-
-    def test_addition(self):
-        assert 4 == Calculate.add(2, 2)
-
-    def test_subtraction(self):
-        assert 2 == Calculate.subtract(4, 2)
